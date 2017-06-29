f7cb05619778081c1ccc6bc5eb0f63695edbab50
Evgeniy <bea@sgsoft.com.ua>
2017-06-29 12:21:48 +0300

    Initial commit

---
+++ src/Generator.java
@@ -0,0 +1,6 @@
+/**
+ * Created by java_dev on 31.05.17.
+ */
+public interface Generator<T> {
+    T next();
+}
---
+++ src/Print.java
@@ -0,0 +1,14 @@
+/**
+ * Created by java_dev on 31.05.17.
+ */
+public class Print {
+    public static void print(Object object) {
+        System.out.println(object);
+    }
+    public static void print() {
+        System.out.println();
+    }
+    public static void printnb(Object obj) {
+        System.out.print(obj);
+    }
+}
